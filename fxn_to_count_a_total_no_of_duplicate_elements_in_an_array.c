07db0baed868135e16d19cd4fe294d0ebed325e8
Anubhav Kumar <144251654+Anubhavmishra07@users.noreply.github.com>
2025-06-08 23:06:13 +0530

    initial commit

--- fxn_to_count_a_total_no_of_duplicate_elements_in_an_array.c
+++ fxn_to_count_a_total_no_of_duplicate_elements_in_an_array.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
-int duples(int a[], int );
+
+int totalDuplicates(int a[], int n);
+
 int main()
 {
     int n;
-    printf("enter the size of array ");
+    printf("Enter the size of array: ");
     scanf("%d", &n);
     int a[n];
-    printf("enter the values ");
+    printf("Enter the elements: ");
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-   printf("total no. of duplicate elements: %d",duples(a, n));
+
+    printf("Total number of duplicate elements: %d\n", totalDuplicates(a, n));
     return 0;
 }
-int duples(int a[], int n)
-{ int c=0;
-    for (int i=0;i<n-1;i++)
-     {
-        for (int j=i+1;j<n;j++)
-        {if (a[i]==a[j])
-        { for(int j=0;j<i;j++)
-           { if (a[i]==a[j])
-         c--;}
-        c++;}}
-     }
-    return c;
+
+int totalDuplicates(int a[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] == a[j])
+            {
+                count++;
+                break; // Count only once for this value
+            }
+        }
+    }
+    return count;
 }
+
