507a16d9e89565274b8808d5e7196b7f2f7ff7f5
Anubhav Kumar <144251654+Anubhavmishra07@users.noreply.github.com>
2025-07-24 23:46:09 +0530

    intial commit

---
+++ c++/Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    cout << "how are you" << x << "nope";
+    int a, b;
+    cin >> a >> b;
+    cout << a << ;
+    return 0;
+}
